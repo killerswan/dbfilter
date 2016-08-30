5fa8caa7681953df98a326cbce3545458f872d1c
Kevin Cantú <kevincantu@gmail.com>
2016-08-30 01:12:26 -0700

    use Dict{Reader,Writer} to avoid magic number indexing

--- ucmr_filter.py
+++ ucmr_filter.py
@@ -4,11 +4,11 @@ from common import newpathrel
 
 def isEpa537(raw_row):
     'if the MethodID field matches EPA 537'
-    return "EPA 537" in raw_row[15]
+    return "EPA 537" in raw_row['MethodID']
 
 def detected(raw_row):
     'if the analytical results sign indicates data found over the limit'
-    return not "<" in raw_row[16]
+    return not "<" in raw_row['AnalyticalResultsSign']
 
 def zipForPws(raw_row):
     '''
@@ -17,11 +17,11 @@ def zipForPws(raw_row):
     '''
     found_zips = []
     with open(newpathrel('ucmr-3-occurrence-data/UCMR3_ZipCodes.txt'), 'rb') as zips_fh:
-        reader = csv.reader(zips_fh,  dialect='excel-tab')
+        reader = csv.DictReader(zips_fh,  dialect='excel-tab')
         for zip in reader:
-            if zip[0] == raw_row[0]:
+            if zip['PWSID'] == raw_row['PWSID']:
                 'PWSIDs match'
-                found_zips.append(zip[1])
+                found_zips.append(zip['ZIPCODE'])
 
     return found_zips
 
@@ -35,19 +35,39 @@ def showSimpleList(xs):
 if __name__ == '__main__':
     with open(newpathrel('ucmr-3-occurrence-data/UCMR3_All.txt'), 'rb') as ucmr3_filehandle:
         with open('filtered-ucmr3-data.csv', 'wb') as output_filehandle:
-            writer = csv.writer(output_filehandle, dialect='excel-tab')
-            reader = csv.reader(ucmr3_filehandle,  dialect='excel-tab')
+            writer_fieldnames = [
+                'PWSID',
+                'PWSName',
+                'Size',
+                'FacilityID',
+                'FacilityName',
+                'FacilityWaterType',
+                'SamplePointID',
+                'SamplePointName',
+                'SamplePointType',
+                'AssociatedFacilityID',
+                'AssociatedSamplePointID',
+                'CollectionDate',
+                'SampleID',
+                'Contaminant',
+                'MRL',
+                'MethodID',
+                'AnalyticalResultsSign',
+                'AnalyticalResultsValue',
+                'SampleEventCode',
+                'MonitoringRequirement',
+                'Region',
+                'State',
+                'ZipCodes'
+            ]
+            writer = csv.DictWriter(output_filehandle, dialect='excel-tab', fieldnames=writer_fieldnames)
+            reader = csv.DictReader(ucmr3_filehandle,  dialect='excel-tab')
 
             for ii,row in enumerate(reader):
-                if ii == 0:
-                    'column headings'
-                    row.append('ZipCodes')
-                    print row
-                    writer.writerow(row)
 
                 if isEpa537(row) and detected(row):
                     # lookup zip codes
-                    row.append(showSimpleList(zipForPws(row)))
+                    row['ZipCodes'] = showSimpleList(zipForPws(row))
 
                     print row
                     writer.writerow(row)
